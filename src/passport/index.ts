a2ac9fd1261518eecb0bb62cf056976312a42e9a
lp1235_win <huanhuan1235@naver.com>
2024-01-07 01:00:04 +0900

    mod: Modify status codes & Add middleware
    - Modified status codes
    - Added middelware to check login status later
    - Changed error messages to Korean

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -3,6 +3,8 @@ import { Strategy as LocalStrategy } from 'passport-local';
 import { ExtractJwt, Strategy as JWTStrategy } from 'passport-jwt';
 import UserService from '../service/user.service'; // User entity 사용 가능하도록 import
 import { verifyPassword } from '../util/authentication';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '../../.env.dev' });
 
 const passportConfig = {
   usernameField: 'email',
@@ -15,7 +17,10 @@ const passportVerify = async (email: string, password: string, done: any) => {
     const user = await UserService.getUserByEmail(email);
     // 검색된 유저 데이터가 없다면 에러 표시
     if (!user) {
-      done(null, false, { message: '존재하지 않는 사용자 입니다.' });
+      done(null, false, {
+        status: 401,
+        message: '존재하지 않는 사용자 입니다.',
+      });
       return;
     }
     // 검색된 유저 데이터가 있다면 유저 해시된 비밀번호 비교
@@ -26,7 +31,10 @@ const passportVerify = async (email: string, password: string, done: any) => {
       return;
     }
     // 비밀번호가 다를 경우 에러 표시
-    done(null, false, { reason: '올바르지 않은 비밀번호 입니다.' });
+    done(null, false, {
+      status: 401,
+      reason: '올바르지 않은 비밀번호 입니다.',
+    });
   } catch (error) {
     console.error(error);
     done(error);
@@ -35,7 +43,7 @@ const passportVerify = async (email: string, password: string, done: any) => {
 
 const JWTConfig = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: 'jwt-secret-key',
+  secretOrKey: process.env.JWT_SECRET_KEY,
 };
 
 const JWTVerify = async (jwtPayload: any, done: any) => {
